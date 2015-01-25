00cad5e6f3681a24553fa78e06ec95290f6c4512
clement <Clment FUMEY>
2015-01-25 13:23:30 +0100

    Button support to enable or disable temperature sending

--- capteur/main.c
+++ capteur/main.c
@@ -44,9 +44,39 @@
 
 #define MSG_TYPE_TEMPERATURE 0x02
 
-#define NUM_TIMERS 1
+/* ************ */
+/*     Leds     */
+/* ************ */
+
+static int led_green_duration;
+static int led_green_flag;
+
+/* asynchronous */
+static void led_green_blink(int duration)
+{
+    led_green_duration = duration;
+    led_green_flag = 1;
+}
+
+static int led_red_duration;
+static int led_red_flag;
+
+/* asynchronous */
+static void led_red_blink(int duration)
+{
+    led_red_duration = duration;
+    led_red_flag = 1;
+}
+
+/* ************ */
+/*    Timers    */
+/* ************ */
+#define NUM_TIMERS 4
 static uint16_t timer[NUM_TIMERS];
 #define TIMER_SEND_TEMP timer[0]
+#define TIMER_ANTIBOUNCING timer[1]
+#define TIMER_LED_RED_ON timer[2]
+#define TIMER_LED_GREEN_ON timer[3]
 
 void timer_tick_cb() {
     int i;
@@ -61,11 +91,39 @@ void timer_tick_cb() {
 int timer_reached(uint16_t timer, uint16_t count) {
     return (timer >= count);
 }
+/* ************ */
+/*    Button    */
+/* ************ */
+
+#define ANTIBOUNCING_DURATION 10 /* 10 timer counts = 100 ms */
+static int antibouncing_flag;
+static int button_pressed_flag;
+static int send_mode_flag;
+
+void button_pressed_cb()
+{
+    if(antibouncing_flag == 0)
+    {
+        button_pressed_flag = 1;
+        antibouncing_flag = 1;
+        TIMER_ANTIBOUNCING = 0;
+        if (send_mode_flag){
+			led_red_blink(100);
+			send_mode_flag = 0;
+			printf("send_mode_flag = 0\n");
+		}else{
+			send_mode_flag = 1;
+			led_green_blink(100);
+			printf("send_mode_flag = 1\n");
+		}
+    }
+}
 
 
-/*
- * Radio
- */
+
+/* ************ */
+/*    Radio     */
+/* ************ */
 
 static char radio_tx_buffer[PKTLEN];
 
@@ -92,6 +150,7 @@ static void init_message()
 /* to be called from within a protothread */
 static void send_temperature()
 {
+	led_green_blink(10);
     init_message();
     radio_tx_buffer[MSG_BYTE_TYPE] = MSG_TYPE_TEMPERATURE;
     int temperature = adc10_sample_temp();
@@ -100,19 +159,80 @@ static void send_temperature()
     char *pt = (char *) &temperature;
     radio_tx_buffer[MSG_BYTE_CONTENT] = pt[1];
     radio_tx_buffer[MSG_BYTE_CONTENT + 1] = pt[0];
-    led_green_switch();
     radio_send_message();
-    led_red_switch();
 }
 
 /* Protothread contexts */
 
-#define NUM_PT 1
+#define NUM_PT 5
 static struct pt pt[NUM_PT];
 
 /* ************************************************** */
 /* ************************************************** */
 /* ************************************************** */
+static PT_THREAD(thread_button(struct pt *pt))
+{
+    PT_BEGIN(pt);
+
+    while(1)
+    {
+        PT_WAIT_UNTIL(pt, button_pressed_flag == 1);
+        button_pressed_flag = 0;
+    }
+
+
+    PT_END(pt);
+}
+
+static PT_THREAD(thread_antibouncing(struct pt *pt))
+{
+    PT_BEGIN(pt);
+
+    while(1)
+    {
+        PT_WAIT_UNTIL(pt, antibouncing_flag
+          && timer_reached(TIMER_ANTIBOUNCING, ANTIBOUNCING_DURATION));
+        antibouncing_flag = 0;
+    }
+
+    PT_END(pt);
+}
+
+static PT_THREAD(thread_led_green(struct pt *pt))
+{
+    PT_BEGIN(pt);
+
+    while(1)
+    {
+        PT_WAIT_UNTIL(pt, led_green_flag);
+        led_green_on();
+        TIMER_LED_GREEN_ON = 0;
+        PT_WAIT_UNTIL(pt, timer_reached(TIMER_LED_GREEN_ON,
+          led_green_duration));
+        led_green_off();
+        led_green_flag = 0;
+    }
+
+    PT_END(pt);
+}
+
+static PT_THREAD(thread_led_red(struct pt *pt))
+{
+    PT_BEGIN(pt);
+
+    while(1)
+    {
+        PT_WAIT_UNTIL(pt, led_red_flag);
+        led_red_on();
+        TIMER_LED_RED_ON = 0;
+        PT_WAIT_UNTIL(pt, timer_reached(TIMER_LED_RED_ON,
+          led_red_duration));
+        led_red_off();
+        led_red_flag = 0;
+    }
+
+    PT_END(pt);
+}
 
 static PT_THREAD(thread_send_temp(struct pt *pt))
 {
@@ -121,7 +241,7 @@ static PT_THREAD(thread_send_temp(struct pt *pt))
     while(1)
     {
         TIMER_SEND_TEMP = 0;
-        PT_WAIT_UNTIL(pt, timer_reached( TIMER_SEND_TEMP, 1000));
+        PT_WAIT_UNTIL(pt, send_mode_flag && timer_reached( TIMER_SEND_TEMP, 1000));
         send_temperature();
         
     }
@@ -139,21 +259,33 @@ int main(void)
 	watchdog_stop();
 
 	set_mcu_speed_dco_mclk_16MHz_smclk_8MHz();
+	
+	/* led init */
 	leds_init();
-	led_red_on();
-
+	
+	/* timer init */
 	timerA_init();
     timerA_register_cb(&timer_tick_cb);
     timerA_start_milliseconds(10);
-
+    
+	/* uart init */
 	uart_init(UART_9600_SMCLK_8MHZ);
 	printf("Smart GreenHouses: temperature sensor\n\r");
-
+	
+	/* temp init */
 	adc10_start();
 	
+	/* radio init */ //rx init missing
 	spi_init();
 	cc2500_init();
-  
+	
+	/* button init */
+    button_init();
+    button_register_cb(button_pressed_cb);
+    antibouncing_flag = 0;
+    button_pressed_flag = 0;
+    send_mode_flag = 1;
+    
 	/* We search for calibration data in flash */
 	if (*((char *)COEFF_1_ADDR) !=0xFFFF){
 		/* Get calibration data from flash */
@@ -172,9 +304,10 @@ int main(void)
 		adc10_calibrate(0, adc_coeff_2);
 	}
 
-  __enable_interrupt();
+	button_enable_interrupt();
+     __enable_interrupt();
 
-  led_green_on();
+
 	/* protothreads init */
 	int i;
 	for(i = 0; i < NUM_PT; i++)
@@ -184,6 +317,10 @@ int main(void)
 
 	while(1) {
 		thread_send_temp(&pt[0]);
+		thread_button(&pt[1]);
+		thread_antibouncing(&pt[2]);
+		thread_led_red(&pt[3]);
+		thread_led_green(&pt[4]);
 	}
 }
 
